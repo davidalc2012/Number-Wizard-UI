3ce0aed364c42fef92feba1e5fcf12ed26905975
David Alcalde <davidalc2012@gmail.com>
2017-01-09 22:59:36 -0600

    Add all scenes and central script

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Levl load request for: "+name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit request");
+	}
+}
